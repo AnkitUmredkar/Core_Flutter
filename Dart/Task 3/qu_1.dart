02310159c5f38bc75fe57713a8b3ea572d5c7978
AnkitUmredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2024-03-08 21:12:25 +0530

    Update qu_1.dart

--- Dart/Task 3/qu_1.dart
+++ Dart/Task 3/qu_1.dart
@@ -1,4 +1,4 @@
-// Writ a Dart Program to gt an print total thrmplo!s information using usr input and Map 
+// Writ a Dart Program to gt an print total thrmplo!s information using usr input and Map
 //Map datatype(use MapEntry class)
 // Tak low mntion attriuts for on
 // id
@@ -8,11 +8,11 @@
 
 import 'dart:io';
 
-void main() 
+class MapEntry 
 {
   var id, name, age, salary;
 
-  for(int i=0; i<3; i++)
+  void set(int i) 
   {
     stdout.write("\n\nEnter Employee id : ");
     id = stdin.readLineSync()!;
@@ -25,16 +25,31 @@ void main()
 
     stdout.write("Enter Employee Salary : ");
     salary = stdin.readLineSync()!;
-
-    stdout.write("\nEmployee ${i+1} details are Below");
-    Map<String, dynamic> emp1 = {
-      'emp_id'  : id,
-      'emp_name': name,
-      'emp_age' : age,
-      'emp_salary': salary
-    };
-    print(emp1);
   }
 
+  void get(int i) 
+  {
+    stdout.write("\nEmployee ${i + 1} details are Below :\n");
+    Map<String, dynamic> emp = 
+    {
+      'emp_id': id,
+      'emp_name': name,
+      'emp_age': age,
+      'emp_salary': salary
+    };
 
+    print(emp);
+  }
+}
+
+void main() 
+{
+  MapEntry m1 = MapEntry();
+
+  for (int i = 0; i < 3; i++)
+  {
+    m1.set(i);
+    m1.get(i);
+  }
+    
 }
