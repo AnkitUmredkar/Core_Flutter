9f75500905786e2ec507c07d907a0ee831fa7d06
AnkitUmredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2024-03-09 19:23:08 +0530

    Update qu_2.dart

--- Dart/Task 3/qu_2.dart
+++ Dart/Task 3/qu_2.dart
@@ -1,48 +1,29 @@
-//2.Write a Dart Program to print all unique elements from a List. Elements may be  redundant 
+//2. Write a Dart Program to print all unique elements from a List. Elements may be  redundant
 // while entring into a List. Usr can only enter elements of String datatype.
 
 import 'dart:io';
 
-void main()
+void main() 
 {
-    List str = [];
-    List unique = [];
-    int i,j;
+  List <String> str = [];
+  int i;
 
-    stdout.write("Enter the Size : ");
-    int n = int.parse(stdin.readLineSync()!);
+  stdout.write("Enter the Size : ");
+  int n = int.parse(stdin.readLineSync()!);
 
-    for(i=0; i<n; i++)
-    {
-      stdout.write("Enter element : ");
-      str.add(stdin.readLineSync()!);
-      unique.add(1);
-    }
+  for (i = 0; i < n; i++) {
+    stdout.write("Enter element : ");
+    str.add(stdin.readLineSync()!);
+  }
 
-    stdout.write("\n>----Entered List----<\n");
-    for(i=0; i<n; i++)
-    {
-        print("${str[i]}");
-    }
+  stdout.write("\n>----Entered List----<\n");
+  for (i = 0; i < n; i++) {
+    print("${str[i]}");
+  }
 
-    stdout.write("\n>----Unique List----<\n");
-    for(i=0; i<n; i++)
-    {
-      if(unique[i] == 0)
-      {
-        continue;
-      }
-      for(j=i+1; j<n; j++)
-      {
-        if(str[i] == str[j])
-        {
-          unique[i] = 0;
-          unique[j] = 0;
-        }
-      }
-      if(unique[i] == 1)
-      {
-        print(str[i]);
-      }
-    }  
+  Set <String> uniqueSet = str.toSet();
+  str = uniqueSet.toList();
+
+  stdout.write("\n>----Unique List----<\n");
+  str.forEach((element) => print(element));
 }
