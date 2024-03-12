69dabef2f590279341934ba52f0f9c64740826be
AnkitUmredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2024-03-12 18:42:40 +0530

    Update qu_16.dart

--- Dart/Task 2/Logical_Round/qu_16.dart
+++ Dart/Task 2/Logical_Round/qu_16.dart
@@ -4,8 +4,9 @@ import 'dart:io';
 
 void main()
 {
-  List arr = [];
-  int i,j,n,x=1;
+  List<int> arr1 = [];
+  List<int> arr2 = [];
+  int i,j,n,temp,leftRotate,rightRotate;
 
   stdout.write("Enter the Size of an array : ");
   n = int.parse(stdin.readLineSync()!);
@@ -13,16 +14,42 @@ void main()
   for(i=0; i<n; i++)
   {
     stdout.write("Enter a arr[$i] : ");
-    arr.add(int.parse(stdin.readLineSync()!));
+    arr1.add(int.parse(stdin.readLineSync()!));
+    arr2.add(arr1[i]);
   }
-  print(arr);
+  stdout.write("\nArray is : ");
+  print(arr1);
 
-  stdout.write("\nRotated Array is : ");
-  x = n;
-  for(i=0; i<n; i++)
+  stdout.write("Enter the Number of Position to Left-Rotate : ");
+  leftRotate = int.parse(stdin.readLineSync()!);
+
+  for(i=0; i<leftRotate; i++)
   {
-    x--;
-    stdout.write("${arr[x]} ");
+    temp = arr1[0];
+    for(j=0; j<n-1; j++)
+    {
+      arr1[j] = arr1[j+1];
+    }
+    arr1[n-1] = temp;
   }
+
+  stdout.write("\nLeft-Rotated Array : ");
+  print(arr1);
+
+  stdout.write("Enter the Number of Position to Right-Rotate : ");
+  rightRotate = int.parse(stdin.readLineSync()!);
+
+  for(i=0; i<rightRotate; i++)
+  {
+    temp = arr2[n-1];
+    for(j=n-1; j>=1; j--)
+    {
+      arr2[j] = arr2[j-1];
+    }
+    arr2[0] = temp;
+  }
+
+  stdout.write("\nRight-Rotated Array : ");
+  print(arr2);
 
 }
